0b5836922d50f377dff04f1ecf41e216a50e5f1e
nerJll <2360300702@qq.com>
2020-08-26 23:55:23 +0800

    详情页分析

--- src/main/java/com/ner/utils/DataAnalysisUtil.java
+++ src/main/java/com/ner/utils/DataAnalysisUtil.java
@@ -1,6 +1,7 @@
 package com.ner.utils;
 
 import com.ner.common.exception.BizException;
+import com.ner.entity.SizeAnalysis;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
@@ -16,9 +17,11 @@ import java.util.List;
  * @date 2020/8/24
  */
 public final class DataAnalysisUtil {
-    //天猫搜索前缀、后缀
-    private final static String TMALLURL_PREFFIX = "https://list.tmall.com/search_product.htm?q=";
-    private final static String TMALLURL_SUFFFIX = "&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100&from=mallfp..pc_1_searchbutton";
+    //天猫一级搜索前缀、后缀
+    private final static String TMALL_ONELEVEL_URLPREFFIX = "https://list.tmall.com/search_product.htm?q=";
+    private final static String TMALL_ONELEVEL_URLSUFFFIX = "&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100&from=mallfp..pc_1_searchbutton";
+    //天猫二级搜索前缀
+    private final static String TMALL_TWOLEVEL_URLPREFFIX = "https:";
 
     //模拟点击链接获取页面html
     private static Document getHtmlByUrl(String url) {
@@ -33,12 +36,12 @@ public final class DataAnalysisUtil {
         return doc;
     }
 
-    //天猫一级搜索（执行商品搜索）,返回商品链接列表
+    //天猫一级搜索（商品搜索）,返回商品链接列表
     private static List<String> tmallOneLevelSearch(String keyWords) {
         //一级搜索链接
-        String url = new StringBuilder(TMALLURL_PREFFIX)
+        String url = new StringBuilder(TMALL_ONELEVEL_URLPREFFIX)
                 .append(keyWords)
-                .append(TMALLURL_SUFFFIX)
+                .append(TMALL_ONELEVEL_URLSUFFFIX)
                 .toString();
         //产品链接，一页60
         List<String> urlList = new ArrayList<>(60);
@@ -47,22 +50,36 @@ public final class DataAnalysisUtil {
         //可以通过元素的标签获取html中的特定元素
         Elements es = doc.getElementsByClass("productImg");
         for (Element e : es) {
-            Attributes as = e.attributes();
-            String href = as.get("href");
-            if (href.length() > 2)
-                urlList.add(href.substring(2));
+            urlList.add(e.attributes().get("href"));
         }
         return urlList;
     }
 
-    //天猫二级搜索（点击商品链接）
-    private static String tmallTwoLevelSearch(String url) {
+    //天猫二级搜索(详情页）
+    private static List<SizeAnalysis> tmallTwoLevelSearch(String url) {
+        String prodUrl = new StringBuilder(TMALL_TWOLEVEL_URLPREFFIX)
+                .append(url)
+                .toString();
+        Document doc = getHtmlByUrl(prodUrl);
+        //产品名称
+        Element prodName = doc.getElementsByTag("h1").get(1);
+        System.out.println(prodName.text());
+        //产品尺寸
+        Element prodSize = doc.getElementById("bd")
+                .getElementById("J_Reviews");
+        System.out.println(prodSize.text());
         return null;
     }
 
-    //天猫商品分析
+    //天猫三级查询（评论翻页）
+    private static List<SizeAnalysis> tmallThreeLevelSearch(String url) {
+        return null;
+    }
+
     public static void analysisTmall(String keyWords) {
         List<String> urlList = tmallOneLevelSearch(keyWords);
-        System.out.println(urlList.toString());
+        for (String url : urlList) {
+            tmallTwoLevelSearch(url);
+        }
     }
 }
