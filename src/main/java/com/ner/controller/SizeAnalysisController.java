d8abbc6be0c2c19171f04ec320e8910db9d9bbe6
nerJll <2360300702@qq.com>
2020-08-31 21:40:02 +0800

    页面数据调试

--- src/main/java/com/ner/controller/SizeAnalysisController.java
+++ src/main/java/com/ner/controller/SizeAnalysisController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @autor jiangll
@@ -19,8 +22,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/size/analysis")
 public class SizeAnalysisController extends BaseController<SizeAnalysisServiceImpl, SizeAnalysisMapper, SizeAnalysis, Long> {
+    private final RedisTemplate<String, Object> redisTemplate;
+
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    public SizeAnalysisController(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/index")
+    public ApiResult index() {
+        //柱状图数据
+        Map<String, Object> column = new HashMap<>(4);
+        column.put("title", "京东bra罩杯分析");
+        column.put("leData", Arrays.asList("北京", "上海", "深圳"));
+        column.put("xData", Arrays.asList("A杯", "B杯", "C杯", "D杯", "E杯"));
+        column.put("sData", Arrays.asList(
+                Arrays.asList(559, 366, 229, 339, 324),
+                Arrays.asList(223, 556, 333, 222, 123),
+                Arrays.asList(433, 566, 122, 211, 97)
+        ));
+        //饼状图数据
+        Map<String, Object> cake = new HashMap<>(3);
+        cake.put("title", "京东bra罩杯分析");
+        cake.put("leData", Arrays.asList("A杯", "B杯", "C杯", "D杯", "E杯"));
+        cake.put("sData", Arrays.asList(559, 366, 229, 339, 324));
+
+        Map<String, Map> result = new HashMap<>(2);
+        result.put("column", column);
+        result.put("cake", cake);
+        return ApiResult.okData(result);
+    }
 
     //修改cookie 1-天猫，2-京东
     @PutMapping("/updCookie")
--- src/main/resources/static/js/dataanalysis.js
+++ src/main/resources/static/js/dataanalysis.js
@@ -1,1 +1,98 @@
-alert(1);
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: baseUrl + '/size/analysis/index',
+        dataType: 'json',
+        success: function (data) {
+            if (data.success) {
+                var column = data.data.column;
+                var cake = data.data.cake;
+                initColumn(column.title,column.leData,column.xData,column.sData);
+                initCake(cake.title,cake.leData,cake.sData);
+            } else {
+                initColumn();
+                initCake();
+            }
+        }
+    });
+});
+
+//柱状图
+function initColumn(title, leData, xDate, sData) {
+    // 基于准备好的dom，初始化echarts实例
+    var column = echarts.init(document.getElementById('column'));
+    // 指定图表的配置项和数据
+    var cop = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data: ['销量1', '销量2', '销量3']
+        },
+        xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量1',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }, {
+            name: '销量2',
+            type: 'bar',
+            data: [3, 21, 32, 6, 11, 18]
+        }, {
+            name: '销量3',
+            type: 'bar',
+            data: [4, 18, 38, 12, 7, 21]
+        }
+        ]
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    column.setOption(cop);
+}
+
+//饼状图
+function initCake(title, leData, sData) {
+    var cake = echarts.init(document.getElementById("cake"));
+    var cap = {
+        title: {
+            text: '某站点用户访问来源',
+            subtext: '纯属虚构',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+            {
+                name: '罩杯',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    {value: 335, name: '直接访问'},
+                    {value: 310, name: '邮件营销'},
+                    {value: 234, name: '联盟广告'},
+                    {value: 135, name: '视频广告'},
+                    {value: 1548, name: '搜索引擎'}
+                ],
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    cake.setOption(cap);
+}
